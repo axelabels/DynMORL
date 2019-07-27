0bbc8614cec4460294622c255f55ed28e8d881e3
unknown <axelabels@gmail.com>
2019-07-27 18:28:43 +0200

    added ccs and pcs computation methods

--- minecart.py
+++ minecart.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+from scipy.spatial import ConvexHull
 import math
 import random
 from utils import truncated_mean, compute_angle, pareto_filter
@@ -48,7 +49,8 @@ ACT_RIGHT = 2
 ACT_ACCEL = 3
 ACT_BRAKE = 4
 ACT_NONE = 5
-FUEL_LIST = [FUEL_MINE + FUEL_IDLE,FUEL_IDLE,FUEL_IDLE,FUEL_IDLE + FUEL_ACC ,FUEL_IDLE,FUEL_IDLE]
+FUEL_LIST = [FUEL_MINE + FUEL_IDLE, FUEL_IDLE, FUEL_IDLE,
+             FUEL_IDLE + FUEL_ACC, FUEL_IDLE, FUEL_IDLE]
 FUEL_DICT = {ACT_MINE: FUEL_MINE + FUEL_IDLE, ACT_LEFT: FUEL_IDLE, ACT_RIGHT: FUEL_IDLE,
              ACT_ACCEL: FUEL_IDLE + FUEL_ACC, ACT_BRAKE: FUEL_IDLE, ACT_NONE: FUEL_IDLE}
 ACTIONS = ["Mine", "Left", "Right", "Accelerate", "Brake", "None"]
@@ -123,7 +125,8 @@ class Mine():
         for i, dist in enumerate(self.distributions):
             mean, std = dist.mean(), dist.std()
             means[i] = truncated_mean(mean, std, 0, float("inf"))
-
+            if np.isnan(means[i]):
+                means[i] = 0
         return means
 
 
@@ -225,114 +228,173 @@ class Minecart:
     def obj_cnt(self):
         return self.ore_cnt + 1
 
-    def optimal_reward(self, frame_skip=1, discount=0.98, add_steps=False, incremental_frame_skip=True):
+    def convex_coverage_set(self, frame_skip=1, discount=0.98, incremental_frame_skip=True, symmetric=True):
+        """
+            Computes an approximate convex coverage set
 
+            Keyword Arguments:
+                frame_skip {int} -- How many times each action is repeated (default: {1})
+                discount {float} -- Discount factor to apply to rewards (default: {1})
+                incremental_frame_skip {bool} -- Wether actions are repeated incrementally (default: {1})
+                symmetric {bool} -- If true, we assume the pattern of accelerations from the base to the mine is the same as from the mine to the base (default: {True})
+
+            Returns:
+                The convex coverage set 
+        """
+        policies = self.pareto_coverage_set(
+            frame_skip, discount, incremental_frame_skip, symmetric)
+        origin = np.min(policies, axis=0)
+        extended_policies = [origin] + policies
+        return [policies[idx - 1] for idx in ConvexHull(extended_policies).vertices if idx != 0]
+
+    def pareto_coverage_set(self, frame_skip=1, discount=0.98, incremental_frame_skip=True, symmetric=True):
+        """
+            Computes an approximate pareto coverage set
+
+            Keyword Arguments:
+                frame_skip {int} -- How many times each action is repeated (default: {1})
+                discount {float} -- Discount factor to apply to rewards (default: {1})
+                incremental_frame_skip {bool} -- Wether actions are repeated incrementally (default: {1})
+                symmetric {bool} -- If true, we assume the pattern of accelerations from the base to the mine is the same as from the mine to the base (default: {True})
+
+            Returns:
+                The pareto coverage set
+        """
         all_rewards = []
-        for mine in (self.mines):
+        base_perimeter = BASE_RADIUS * BASE_SCALE
+
+        # Empty mine just outside the base
+        virtual_mine = Mine(self.ore_cnt, (base_perimeter**2 / 2)
+                            ** (1 / 2), (base_perimeter**2 / 2)**(1 / 2))
+        virtual_mine.distributions = [
+            scipy.stats.norm(0, 0)
+            for _ in range(self.ore_cnt)
+        ]
+        for mine in (self.mines + [virtual_mine]):
             mine_distance = mag(mine.pos - HOME_POS) - \
                 MINE_RADIUS * MINE_SCALE - BASE_RADIUS * BASE_SCALE / 2
-
-            # Slowest (rational) way of reaching mine is to accelerate once then
-            # do nothing, we first compute how many suche idle actions it takes
-            # to reach the mine
-            max_idles = 0
-
-            dist = mine_distance
-            speed = 0
-
-            steps = frame_skip if incremental_frame_skip else 1
-            step_size = 1 if incremental_frame_skip else frame_skip
-            # initial acceleration
-            for _ in range(steps):
-                speed += ACCELERATION * step_size
-                dist -= speed * step_size
-            # subsequent idle actions
-            while dist > 0:
-                for _ in range(steps):
-                    dist -= speed * step_size
-
-                max_idles += 1
 
             # Number of rotations required to face the mine
             angle = compute_angle(mine.pos, HOME_POS, [1, 1])
             rotations = int(ceil(abs(angle) / (ROTATION * frame_skip)))
 
-            # all possible action sequences that lead to the mine (after the initial acceleration)
-            sequences = itertools.product([3, 5], repeat=max_idles - 1)
+            # Build pattern of accelerations/nops to reach the mine
+            # initialize with single acceleration
+            queue = [{"speed": ACCELERATION * frame_skip, "dist": mine_distance - frame_skip *
+                      (frame_skip + 1) / 2 * ACCELERATION if incremental_frame_skip else mine_distance - ACCELERATION * frame_skip * frame_skip, "seq": [ACT_ACCEL]}]
+            trimmed_sequences = []
 
-            # Some of these sequences would go beyond the mine, trim to keep
-            # only unique actions until the mine is reached
-            trimmed_sequences = set()
-            for p in sequences:
-                dist = mine_distance
-                speed = 0
-                # Initial acceleration
-                for _ in range(steps):
-                    speed += ACCELERATION * step_size
-                    dist -= speed * step_size
-                # Subsequent actions
-                for i, a in enumerate(p):
-                    for _ in range(steps):
-                        if a == ACT_ACCEL:
-                            speed += ACCELERATION * step_size
-                        dist -= speed * step_size
+            while len(queue) > 0:
+                seq = queue.pop()
+                # accelerate
+                new_speed = seq["speed"] + ACCELERATION * frame_skip
+                accelerations = new_speed / ACCELERATION
+                movement = accelerations * (accelerations + 1) / 2 * ACCELERATION - (
+                    accelerations - frame_skip) * ((accelerations - frame_skip) + 1) / 2 * ACCELERATION
+                dist = seq["dist"] - movement
+                speed = new_speed
+                if dist <= 0:
+                    trimmed_sequences.append(seq["seq"] + [ACT_ACCEL])
+                else:
+                    queue.append({"speed": speed, "dist": dist,
+                                  "seq": seq["seq"] + [ACT_ACCEL]})
+                # idle
+                dist = seq["dist"] - seq["speed"] * frame_skip
 
-                    if dist <= 0:
-                        break
-                
-                trimmed_sequences.add(p[:i + 1])
-                
-            # Actions performed before mining
-            # 1. rotation towards mine
-            # 2. acceleration+nops (= ACT_IDLE)
-            # 3. brake
-            # 4. rotation to face origin
-            pre_mine_actions = []
-            for p in trimmed_sequences:
-                pre_mine_actions.append([ACT_LEFT] * rotations +  # rotations to face mine
-                                        [ACT_ACCEL] +  # initial acceleration
-                                        list(p) +  # accelerate/idle sequence
-                                        [ACT_BRAKE] +  # brake when on mine
-                                        [ACT_LEFT] * (180 // (ROTATION * frame_skip)))  # rotate
+                if dist <= 0:
+                    trimmed_sequences.append(seq["seq"] + [ACT_NONE])
+                else:
+                    queue.append(
+                        {"speed": seq["speed"], "dist": dist, "seq": seq["seq"] + [ACT_NONE]})
 
-            print("building mine")
+            # Build rational mining sequences
             mine_means = mine.distribution_means() * frame_skip
             mn_sum = np.sum(mine_means)
             # on average it takes up to this many actions to fill cart
-            max_mine_actions = int(ceil(self.capacity / mn_sum))
+            max_mine_actions = 0 if mn_sum == 0 else int(
+                ceil(self.capacity / mn_sum))
 
             # all possible mining sequences (i.e. how many times we mine)
             mine_sequences = [[ACT_MINE] *
                               i for i in range(1, max_mine_actions + 1)]
 
-            print("building post")
-            # Action to perform after mining is just returning to base,
-            # i.e., accelerating and then any sequence of acceleration and nops
-            post_mine_actions = []
-            for p in trimmed_sequences:
-                post_mine_actions.append([ACT_ACCEL] + list(p))
-
-            print("combining")
             # All possible combinations of actions before, during and after mining
-            all_sequences = map(lambda pre_intra_post: list(pre_intra_post[0]) + list(pre_intra_post[1]) + list(
-                pre_intra_post[2]), (itertools.product(pre_mine_actions, mine_sequences, post_mine_actions)))
+            if len(mine_sequences) > 0:
+                if not symmetric:
+                    all_sequences = map(
+                        lambda sequences: list(sequences[0]) + list(sequences[1]) + list(
+                            sequences[2]) + list(
+                            sequences[3]) + list(
+                            sequences[4]),
+                        itertools.product([[ACT_LEFT] * rotations],
+                                          trimmed_sequences,
+                                          [[ACT_BRAKE] + [ACT_LEFT] *
+                                              (180 // (ROTATION * frame_skip))],
+                                          mine_sequences,
+                                          trimmed_sequences)
+                    )
 
-            print("computing rewards")
+                else:
+                    all_sequences = map(
+                        lambda sequences: list(sequences[0]) + list(sequences[1]) + list(
+                            sequences[2]) + list(
+                            sequences[3]) + list(
+                            sequences[1]),
+                        itertools.product([[ACT_LEFT] * rotations],
+                                          trimmed_sequences,
+                                          [[ACT_BRAKE] + [ACT_LEFT] *
+                                              (180 // (ROTATION * frame_skip))],
+                                          mine_sequences)
+                    )
+            else:
+                if not symmetric:
+                    print([ACT_NONE] + trimmed_sequences[1:],
+                          trimmed_sequences[1:], trimmed_sequences)
+                    all_sequences = map(
+                        lambda sequences: list(sequences[0]) + list(sequences[1]) + list(
+                            sequences[2]) + [ACT_NONE] + list(
+                            sequences[3])[1:],
+                        itertools.product([[ACT_LEFT] * rotations],
+                                          trimmed_sequences,
+                                          [[ACT_LEFT] *
+                                              (180 // (ROTATION * frame_skip))],
+                                          trimmed_sequences)
+                    )
+
+                else:
+                    all_sequences = map(
+                        lambda sequences: list(sequences[0]) + list(sequences[1]) + list(
+                            sequences[2]) + [ACT_NONE] + list(
+                            sequences[1][1:]),
+                        itertools.product([[ACT_LEFT] * rotations],
+                                          trimmed_sequences,
+                                          [[ACT_LEFT] * (180 // (ROTATION * frame_skip))])
+                    )
+
             # Compute rewards for each sequence
+            fuel_costs = np.array([f * frame_skip for f in FUEL_LIST])
+
+            def maxlen(l):
+                if len(l) == 0:
+                    return 0
+                return max([len(s) for s in l])
+
+            longest_pattern = maxlen(trimmed_sequences)
+            max_len = rotations + longest_pattern + 1 + \
+                (180 // (ROTATION * frame_skip)) + \
+                maxlen(mine_sequences) + longest_pattern
+            discount_map = discount**np.arange(max_len)
             for s in all_sequences:
-                reward = np.zeros(self.obj_cnt())
-                for i, a in enumerate(s):
-                    if a == ACT_MINE:
-                        rem_cap = self.capacity - np.sum(reward[:-1])
-                        mined_ratio = min(1, rem_cap / mn_sum)
-                        step_mined = mine_means * mined_ratio
-                        reward[:-1] += step_mined
-                    reward[-1] += FUEL_LIST[a] * frame_skip * discount**i
-                # collected resources rewarded at the end
-                reward[:-1] *= discount**i
+                reward = np.zeros((len(s), self.obj_cnt()))
+                reward[:, -1] = fuel_costs[s]
+                mine_actions = s.count(ACT_MINE)
+                reward[-1, :-1] = mine_means * mine_actions / \
+                    max(1, (mn_sum * mine_actions) / self.capacity)
+
+                reward = np.dot(discount_map[:len(s)], reward)
                 all_rewards.append(reward)
-        print("computing pareto")
-        all_rewards = pareto_filter(all_rewards, minimize=False)
+
+            all_rewards = pareto_filter(all_rewards, minimize=False)
 
         return all_rewards
 
@@ -546,7 +608,7 @@ class Minecart:
         }
 
     def reset(self):
-        """Reset's the environment to the start state
+        """Resets the environment to the start state
 
         Returns:
             [type] -- [description]
